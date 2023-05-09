67515ccff15bd74b84b16a51355aabed637463cc
Consistent_Milk <mahmudulhasanfh91@gmail.com>
2023-05-09 19:56:42 +0600

    use keyword

--- _7_crates_and_packages/_2_modules/Readme.md
+++ _7_crates_and_packages/_2_modules/Readme.md
@@ -1,7 +1,6 @@
 Module structure used in this crate:
 
 (The first underscore in function names were used to get rid of warning from rust-analyzer)
-
 crate
 
 └── front_of_house
--- _7_crates_and_packages/_4_use_keyword/src/main.rs
+++ _7_crates_and_packages/_4_use_keyword/src/main.rs
@@ -1,3 +1,12 @@
+use rand::Rng;
+use std::collections::HashMap;
+
 fn main() {
-    println!("Hello, world!");
+    let mut map: HashMap<i32, i32> = HashMap::new();
+    map.insert(1, 2);
+
+    let secret_number: i32 = rand::thread_rng().gen_range(1..=100);
+
+    println!("The value stored in key 1 is: {}", map[&1]);
+    println!("Randomly generated value using rand is: {}", secret_number);
 }
