c207c8315dc73cabde2d5075df14d9c5af4816c1
Consistent_Milk <mahmudulhasanfh91@gmail.com>
2023-05-11 13:36:43 +0600

    leetcode 001, 005

--- Leetcode/_001_two_sum/src/main.rs
+++ Leetcode/_001_two_sum/src/main.rs
@@ -5,14 +5,18 @@ use std::collections::HashMap;
 impl Solution {
     fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
         let mut hm: HashMap<i32, i32> = HashMap::new();
+
         for (i, &num) in nums.iter().enumerate() {
-            if let Some(&j) = hm.get(&(target - num)) {
-                return vec![j, i as i32];
-            } else {
-                hm.insert(num, i as i32);
+            let check: Option<&i32> = hm.get(&(target - num));
+
+            match check {
+                Some(&j) => return vec![j, i as i32],
+                None => (),
             }
+
+            hm.insert(num, i as i32);
         }
-        vec![]
+        return vec![-1, -1];
     }
 }
 
--- Leetcode/_005_longest_palindromic_substring/src/main.rs
+++ Leetcode/_005_longest_palindromic_substring/src/main.rs
@@ -1,3 +1,51 @@
+struct Solution;
+
+use std::iter::FromIterator;
+
+impl Solution {
+    fn longest_palindrome(s: String) -> String {
+        let n: usize = s.len();
+        if n == 0 {
+            return "".to_string();
+        }
+
+        let s: Vec<char> = s.chars().collect();
+        let mut start: usize = 0;
+        let mut end: usize = 0;
+
+        for i in 0..n {
+            let mut left: usize = i;
+            let mut right: usize = i;
+
+            while right + 1 < n && s[right + 1] == s[left] {
+                right += 1;
+            }
+
+            while left > 0 && right < n - 1 && s[left - 1] == s[right + 1] {
+                left -= 1;
+                right += 1;
+            }
+
+            if right - left > end - start {
+                start = left;
+                end = right;
+            }
+        }
+        String::from_iter(&s[start..=end])
+    }
+}
+
+fn test() -> bool {
+    let s = "babad".to_string();
+    let res = "bab".to_string();
+    return Solution::longest_palindrome(s) == res;
+}
+
 fn main() {
-    println!("Hello, world!");
+    let result: bool = test();
+
+    match result {
+        true => println!("Test Passed."),
+        _ => println!("Test Failed."),
+    }
 }
