6a86c35e197c81fdbe8d0a3d46694f4e8f8e678b
Consistent_Milk <mahmudulhasanfh91@gmail.com>
2023-05-21 16:39:30 +0600

    Refractor

--- Leetcode/_001_two_sum/src/main.rs
+++ Leetcode/_001_two_sum/src/main.rs
@@ -7,20 +7,16 @@ impl Solution {
         let mut hm: HashMap<i32, i32> = HashMap::new();
 
         for (i, &num) in nums.iter().enumerate() {
-            if let Some(&j) = hm.get(&(target - num)) {
-                return vec![j, i as i32];
-            } else {
-                hm.insert(num, i as i32);
+            let complement: i32 = target - num;
+
+            let check: Option<&i32> = hm.get(&complement);
+
+            match check {
+                Some(&j) => return vec![j, i as i32],
+                None => (),
             }
 
-            // let check: Option<&i32> = hm.get(&(target - num));
-
-            // match check {
-            //     Some(&j) => return vec![j, i as i32],
-            //     None => (),
-            // }
-
-            // hm.insert(num, i as i32);
+            hm.insert(num, i as i32);
         }
         return vec![-1, -1];
     }
@@ -39,4 +35,18 @@ fn main() {
         true => println!("Test passed."),
         _ => println!("Test failed."),
     }
+}
+
+#[test]
+fn test_1() {
+    let nums: Vec<i32> = vec![2, 7, 11, 15];
+    let target: i32 = 9;
+    assert_eq!(Solution::two_sum(nums, target), [0, 1]);
+}
+
+#[test]
+fn test_2() {
+    let nums: Vec<i32> = vec![3, 5, 7];
+    let target: i32 = 6;
+    assert_eq!(Solution::two_sum(nums, target), [-1, -1]);
 }
